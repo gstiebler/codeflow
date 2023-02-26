e460cfb13dd7af5b2b66acdecc1558685bd5aff6
Guilherme Stiebler <guilhermemst@gmail.com>
2023-02-25 17:44:33 -0800

    Revert "testing bad commit to check Github Actions"
    This reverts commit 94685471eda5c19e486c2964e109f90e94414361.

--- app/src/test/kotlin/codeflow/AppTest.kt
+++ app/src/test/kotlin/codeflow/AppTest.kt
@@ -7,14 +7,12 @@ import codeflow.java.AstReader
 import java.nio.file.Path
 import kotlin.test.Test
 import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 
 class AppTest {
     @Test fun appHasAGreeting() {
         val classUnderTest = App()
         assertNotNull(classUnderTest.greeting, "app should have a greeting")
-        assertTrue(false)
     }
 
     @Test fun codeflow() {
